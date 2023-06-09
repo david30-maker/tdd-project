df82880d2005883012128a76465e537b4785736a
david30-maker <davidokpah396@gmail.com>
2023-06-09 12:00:05 +0100

    fix linters error

--- solver.rb
+++ solver.rb
@@ -1,32 +1,30 @@
 class Solver
+  def factorial(n)
+    raise ArgumentError, 'Factorial is only defined for non-negative integers' if n.negative?
 
-    def factorial(n)
-        raise ArgumentError, 'Factorial is only defined for non-negative integers' if n < 0
+    return 1 if n.zero?
 
-        return 1 if n.zero?
-
-        result = 1
-        (1..n).each do |i|
-            result *= i
-        end
-        result
+    result = 1
+    (1..n).each do |i|
+      result *= i
     end
+    result
+  end
 
-    def reverse(word)
-        word.reverse
-    end
+  def reverse(word)
+    word.reverse
+  end
 
-    # here is your part of the code, uncomment it and run the code
-    # def fizzbuzz(n)
-    #     if n % 3 == 0 && n % 5 == 0
-    #         'fizzbuzz'
-    #     elsif n % 3 == 0
-    #         'fizz'
-    #     elsif n % 5 == 0
-    #         'buzz'
-    #     else
-    #         n.to_s
-    #     end
-    # end
-
+  # here is your part of the code, uncomment it and run the code
+  # def fizzbuzz(n)
+  #     if n % 3 == 0 && n % 5 == 0
+  #         'fizzbuzz'
+  #     elsif n % 3 == 0
+  #         'fizz'
+  #     elsif n % 5 == 0
+  #         'buzz'
+  #     else
+  #         n.to_s
+  #     end
+  # end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,49 +2,51 @@ require 'rspec'
 require_relative '../solver'
 
 RSpec.describe Solver do
-describe '#factorial' do
+  describe '#factorial' do
     it 'returns the factorial of a positive integer' do
-        solver = Solver.new
-        expect(solver.factorial(5)).to eq(120)
+      solver = Solver.new
+      expect(solver.factorial(5)).to eq(120)
     end
 
     it 'returns 1 for factorial of 0' do
-        solver = Solver.new
-        expect(solver.factorial(0)).to eq(1)
+      solver = Solver.new
+      expect(solver.factorial(0)).to eq(1)
     end
 
     it 'raises an exception for negative integers' do
-        solver = Solver.new
-        expect { solver.factorial(-5) }.to raise_error(ArgumentError, 'Factorial is only defined for non-negative integers')
+      solver = Solver.new
+      expect do
+        solver.factorial(-5)
+      end.to raise_error(ArgumentError, 'Factorial is only defined for non-negative integers')
     end
- end
+  end
 
-    describe '#reverse' do
-        it 'returns the reversed word' do
-            solver = Solver.new
-            expect(solver.reverse('hello')).to eq('olleh')
-        end
+  describe '#reverse' do
+    it 'returns the reversed word' do
+      solver = Solver.new
+      expect(solver.reverse('hello')).to eq('olleh')
     end
-# here is your part of the code, uncomment it and run the test
-#     describe '#fizzbuzz' do
-#         it 'returns "fizz" for numbers divisible by 3' do
-#             solver = Solver.new
-#             expect(solver.fizzbuzz(9)).to eq('fizz')
-#         end
+  end
+  # here is your part of the code, uncomment it and run the test
+  #     describe '#fizzbuzz' do
+  #         it 'returns "fizz" for numbers divisible by 3' do
+  #             solver = Solver.new
+  #             expect(solver.fizzbuzz(9)).to eq('fizz')
+  #         end
 
-#         it 'returns "buzz" for numbers divisible by 5' do
-#             solver = Solver.new
-#             expect(solver.fizzbuzz(10)).to eq('buzz')
-#         end
+  #         it 'returns "buzz" for numbers divisible by 5' do
+  #             solver = Solver.new
+  #             expect(solver.fizzbuzz(10)).to eq('buzz')
+  #         end
 
-#         it 'returns "fizzbuzz" for numbers divisible by 3 and 5' do
-#             solver = Solver.new
-#             expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
-#         end
+  #         it 'returns "fizzbuzz" for numbers divisible by 3 and 5' do
+  #             solver = Solver.new
+  #             expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+  #         end
 
-#         it 'returns the number as a string for other cases' do
-#             solver = Solver.new
-#             expect(solver.fizzbuzz(7)).to eq('7')
-#         end
-#     end
-# end
+  #         it 'returns the number as a string for other cases' do
+  #             solver = Solver.new
+  #             expect(solver.fizzbuzz(7)).to eq('7')
+  #         end
+  #     end
+end
