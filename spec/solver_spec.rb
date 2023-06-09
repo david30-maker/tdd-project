cf1e324d0329affdce5d488a8b77de4977fd6589
david30-maker <davidokpah396@gmail.com>
2023-06-09 11:29:29 +0100

    tdd test best practice

---
+++ spec/solver_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require_relative '../solver'
+
+RSpec.describe Solver do
+    describe '#factorial' do
+    it 'returns the factorial of a positive integer' do
+        solver = Solver.new
+        expect(solver.factorial(5)).to eq(120)
+    end
+
+    it 'returns 1 for factorial of 0' do
+        solver = Solver.new
+        expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'raises an exception for negative integers' do
+        solver = Solver.new
+        expect { solver.factorial(-5) }.to raise_error(ArgumentError, 'Factorial is only defined for non-negative integers')
+    end
+end
