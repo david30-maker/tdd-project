5abab2ba560f2870c1d4f0ca5993cd714f60dd3a
david30-maker <davidokpah396@gmail.com>
2023-06-09 11:41:26 +0100

    reverse method test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../solver'
 
 RSpec.describe Solver do
-    describe '#factorial' do
+describe '#factorial' do
     it 'returns the factorial of a positive integer' do
         solver = Solver.new
         expect(solver.factorial(5)).to eq(120)
@@ -17,4 +17,14 @@ RSpec.describe Solver do
         solver = Solver.new
         expect { solver.factorial(-5) }.to raise_error(ArgumentError, 'Factorial is only defined for non-negative integers')
     end
+ end
+
+    describe '#reverse' do
+        it 'returns the reversed word' do
+            solver = Solver.new
+            expect(solver.reverse('hello')).to eq('olleh')
+        end
+    end
+
+
 end
