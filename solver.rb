a38366804e37976fe5bf5e90075bdd671b53da2c
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-06-09 13:14:23 +0100

    Solve linter issues

--- solver.rb
+++ solver.rb
@@ -1,13 +1,10 @@
 class Solver
-  def factorial(n)
-    raise ArgumentError, 'Factorial is only defined for non-negative integers' if n.negative?
-
-    return 1 if n.zero?
-
+  def factorial(number)
     result = 1
-    (1..n).each do |i|
-      result *= i
-    end
+    return result if [0, 1].include?(number)
+    raise StandardError, 'Number cannot be negative' if number.negative?
+
+    (1..number).each { |i| result *= i }
     result
   end
 
@@ -15,15 +12,15 @@ class Solver
     word.reverse
   end
 
-  def fizzbuzz(n)
-      if n % 3 == 0 && n % 5 == 0
-          'fizzbuzz'
-      elsif n % 3 == 0
-          'fizz'
-      elsif n % 5 == 0
-          'buzz'
-      else
-          n.to_s
-      end
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,51 +1,38 @@
-require 'rspec'
 require_relative '../solver'
 
-RSpec.describe Solver do
+describe Solver do
   describe '#factorial' do
-    it 'returns the factorial of a positive integer' do
-      solver = Solver.new
+    solver = Solver.new
+    it 'should raise an exception when number is negative' do
+      expect { solver.factorial(-2) }.to raise_error(StandardError, 'Number cannot be negative')
+    end
+    it 'returns the factorial of the number' do
+      expect(solver.factorial(1)).to eq(1)
       expect(solver.factorial(5)).to eq(120)
-    end
-
-    it 'returns 1 for factorial of 0' do
-      solver = Solver.new
       expect(solver.factorial(0)).to eq(1)
-    end
-
-    it 'raises an exception for negative integers' do
-      solver = Solver.new
-      expect do
-        solver.factorial(-5)
-      end.to raise_error(ArgumentError, 'Factorial is only defined for non-negative integers')
     end
   end
 
   describe '#reverse' do
-    it 'returns the reversed word' do
-      solver = Solver.new
+    solver = Solver.new
+    it 'should reverse the given word' do
       expect(solver.reverse('hello')).to eq('olleh')
     end
   end
-    describe '#fizzbuzz' do
-        it 'returns "fizz" for numbers divisible by 3' do
-            solver = Solver.new
-            expect(solver.fizzbuzz(9)).to eq('fizz')
-        end
 
-        it 'returns "buzz" for numbers divisible by 5' do
-            solver = Solver.new
-            expect(solver.fizzbuzz(10)).to eq('buzz')
-        end
-
-        it 'returns "fizzbuzz" for numbers divisible by 3 and 5' do
-            solver = Solver.new
-            expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
-        end
-
-        it 'returns the number as a string for other cases' do
-            solver = Solver.new
-            expect(solver.fizzbuzz(7)).to eq('7')
-        end
+  describe '#fizzbuzz' do
+    solver = Solver.new
+    it 'When N is divisible by 3 and 5, return "fizzbuzz"' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
     end
+    it 'When N is divisible by 3 , return "fizz"' do
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+    it 'When N is divisible by 5 , return "buzz"' do
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+    it 'Any other case, return N as a string' do
+      expect(solver.fizzbuzz(2)).to eq('2')
+    end
+  end
 end
