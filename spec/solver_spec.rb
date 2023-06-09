4a51aaa014f2fb4a3e1c783763e2c3f8e488d7b2
david30-maker <davidokpah396@gmail.com>
2023-06-09 11:48:37 +0100

    linter error fix

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -25,6 +25,26 @@ describe '#factorial' do
             expect(solver.reverse('hello')).to eq('olleh')
         end
     end
+# here is your part of the code, uncomment it and run the test
+#     describe '#fizzbuzz' do
+#         it 'returns "fizz" for numbers divisible by 3' do
+#             solver = Solver.new
+#             expect(solver.fizzbuzz(9)).to eq('fizz')
+#         end
 
+#         it 'returns "buzz" for numbers divisible by 5' do
+#             solver = Solver.new
+#             expect(solver.fizzbuzz(10)).to eq('buzz')
+#         end
 
-end
+#         it 'returns "fizzbuzz" for numbers divisible by 3 and 5' do
+#             solver = Solver.new
+#             expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+#         end
+
+#         it 'returns the number as a string for other cases' do
+#             solver = Solver.new
+#             expect(solver.fizzbuzz(7)).to eq('7')
+#         end
+#     end
+# end
