ee4e754ddf21d479f81a62c605cb92ea9cc42b44
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-06-09 12:49:21 +0100

    Modify solver_spec.rb file for completion of the testing

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -27,26 +27,25 @@ RSpec.describe Solver do
       expect(solver.reverse('hello')).to eq('olleh')
     end
   end
-  # here is your part of the code, uncomment it and run the test
-  #     describe '#fizzbuzz' do
-  #         it 'returns "fizz" for numbers divisible by 3' do
-  #             solver = Solver.new
-  #             expect(solver.fizzbuzz(9)).to eq('fizz')
-  #         end
+    describe '#fizzbuzz' do
+        it 'returns "fizz" for numbers divisible by 3' do
+            solver = Solver.new
+            expect(solver.fizzbuzz(9)).to eq('fizz')
+        end
 
-  #         it 'returns "buzz" for numbers divisible by 5' do
-  #             solver = Solver.new
-  #             expect(solver.fizzbuzz(10)).to eq('buzz')
-  #         end
+        it 'returns "buzz" for numbers divisible by 5' do
+            solver = Solver.new
+            expect(solver.fizzbuzz(10)).to eq('buzz')
+        end
 
-  #         it 'returns "fizzbuzz" for numbers divisible by 3 and 5' do
-  #             solver = Solver.new
-  #             expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
-  #         end
+        it 'returns "fizzbuzz" for numbers divisible by 3 and 5' do
+            solver = Solver.new
+            expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+        end
 
-  #         it 'returns the number as a string for other cases' do
-  #             solver = Solver.new
-  #             expect(solver.fizzbuzz(7)).to eq('7')
-  #         end
-  #     end
+        it 'returns the number as a string for other cases' do
+            solver = Solver.new
+            expect(solver.fizzbuzz(7)).to eq('7')
+        end
+    end
 end
